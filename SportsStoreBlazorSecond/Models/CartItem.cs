4b47c6d4f4e5d141ca0ebc262888ac7e4afae89f
RQ <RQ@BE-NBK-019>
2023-06-13 15:02:07 +0200

    Add project files.

---
+++ SportsStoreBlazorSecond/Models/CartItem.cs
@@ -0,0 +1,11 @@
+﻿namespace SportsStoreBlazorSecond.Models
+{
+    public class CartItem
+    {
+        public int ProductID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+    }
+}
---
+++ SportsStoreBlazorSecond/Models/CartLine.cs
@@ -0,0 +1,8 @@
+﻿namespace SportsStoreBlazorSecond.Models
+{
+    public class CartLine
+    {
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+    }
+}
